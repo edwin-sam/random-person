170818667dc06c54d00ec8811683e9a6d0510bee
Edwin Sam <edwin@edwins-mbp-3.lan>
2022-01-31 23:51:38 -0800

    RDP-1 Add background and name card components
    RDP-1 css indentation fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import FaceCard from './facCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="body-container">
+        <FaceCard />
+      </header>
     </div>
   );
 }
